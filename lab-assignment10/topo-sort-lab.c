12ca781da48acd34a9e6b1c3060fdb21cbfad124
Nicholas Ung <nicholasung22@gmail.com>
2024-07-12 10:34:42 -0700

    Upload lab assignment 10
    Signed-off-by: Nicholas Ung <nicholasung22@gmail.com>

--- lab-assignment10/topo-sort-lab.c
+++ lab-assignment10/topo-sort-lab.c
@@ -1,135 +1,168 @@
-// Enter your name here
-//Enter your email here
+// Name: Nicholas Ung
+// Email: ung.n@northeastern.edu
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #define MAX 50
 
-//define a structure for queue
+// define a structure for queue
 typedef struct
 {
-    int a[MAX];
-    int front,rear;
-}Queue;
+  int a[MAX];
+  int front, rear;
+} Queue;
 
-//isEmpty function
+// isEmpty function
 int isEmpty(int front)
 {
-    if(front==-1)
-        return 1;
-    else
-        return 0;
+  if (front == -1)
+    return 1;
+  else
+    return 0;
 }
 
-//Dequeue function to dequeue a vertex from the queue*/
+// Dequeue function to dequeue a vertex from the queue*/
 int dequeue(Queue *q)
 {
-    
-    int x=q->a[q->front];
-    if(q->front==q->rear)
-    {
-        q->front=q->rear=-1;
-    }
-    else
-     {
-         q->front++;
-     }
-    return x;
+
+  int x = q->a[q->front];
+  if (q->front == q->rear)
+  {
+    q->front = q->rear = -1;
+  }
+  else
+  {
+    q->front++;
+  }
+  return x;
 }
 /* Queue to hold the vertices with in-degree zero*/
 void enqueue(Queue *q, int e)
 {
 
-    q->rear++;
-    q->a[q->rear]=e;
-    if(q->front==-1)
-    {
-        q->front=0;
-    }
+  q->rear++;
+  q->a[q->rear] = e;
+  if (q->front == -1)
+  {
+    q->front = 0;
+  }
 }
 
 /*Create graph in adjacency matrix form*/
-void creategraph(int arr[][MAX],int V)
+void creategraph(int arr[][MAX], int V)
 {
-    int i,j;
-    for(i =1; i<=V; i++)
+  int i, j;
+  for (i = 1; i <= V; i++)
+  {
+    for (j = 1; j <= V; j++)
     {
-        for(j = 1; j<=V; j++)
-        {
-            arr[i][j] = 0;
-        }
+      arr[i][j] = 0;
     }
+  }
 }
 
 /* Function to add edges from source to destination*/
-void addEdge(int arr[][MAX],int src, int dest)
+void addEdge(int arr[][MAX], int src, int dest)
 {
-     arr[src][dest] = 1;
+  arr[src][dest] = 1;
 }
 
 /*Print the adjacency matrix of the Graph*/
-void printAdjMatrix(int arr[][MAX],int V)
+void printAdjMatrix(int arr[][MAX], int V)
 {
-     int i, j;
+  int i, j;
 
-     for(i =1; i<=V; i++)
-     {
-         for(j = 1; j<=V; j++)
-         {
-             printf("%6d ", arr[i][j]);
-         }
-         printf("\n");
-     }
+  for (i = 1; i <= V; i++)
+  {
+    for (j = 1; j <= V; j++)
+    {
+      printf("%6d ", arr[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 /*calculate the indegree of each vertex by looping through all vertices and edges in the adjacency matrix*/
-int find_indegree(int arr[][MAX], int node,int n)
+int find_indegree(int arr[][MAX], int node, int n)
 {
-    int i,in_deg = 0;
-    
-    //insert your code here
-    
+  int i, in_deg = 0;
+
+  // insert your code here
+  for (i = 1; i <= n; i++)
+  {
+    if (arr[i][node] == 1)
+    {
+      in_deg++;
+    }
+  }
+  return in_deg;
 }
 
 /*Function to perform topological sorting of the Graph */
 void topologicalOrder(int arr[][MAX], Queue *t, int n)
 {
-    int j=0,delNode,i;
-    int result[MAX],indeg[MAX];
-    
-    
-    
-    
-    //insert your code here
-    
-    
-    
-    printf("topological ordering\n");
-    for(i=0; i<j;i++)
-        printf("%d\t",result[i]);
-    printf("\n");
-    
+  int j = 0, delNode, i;
+  int result[MAX], indeg[MAX];
+
+  // insert your code here
+
+  // Find indegree of all vertices
+  for (i = 1; i <= n; i++)
+  {
+    indeg[i] = find_indegree(arr, i, n);
+    // Enqueue all vertices with indegree 0
+    if (indeg[i] == 0)
+    {
+      enqueue(t, i);
+    }
+  }
+
+  while (!isEmpty(t->front))
+  {
+    delNode = dequeue(t);
+    result[j++] = delNode;
+
+    // Decrease the indegree of the neighboring vertices
+    for (i = 1; i <= n; i++)
+    {
+      if (arr[delNode][i] == 1)
+      {
+        arr[delNode][i] = 0;
+        indeg[i]--;
+        // If indegree becomes 0, enqueue the vertex
+        if (indeg[i] == 0)
+        {
+          enqueue(t, i);
+        }
+      }
+    }
+  }
+
+  printf("topological ordering\n");
+  for (i = 0; i < j; i++)
+    printf("%d\t", result[i]);
+  printf("\n");
 }
 /*Main Program*/
 int main()
 {
-    int adjMatrix[MAX][MAX];
-    int vertices=7;
-    Queue t;
-    t.front=t.rear=-1;
-    creategraph(adjMatrix,vertices);
-    addEdge(adjMatrix,1,2);
-    addEdge(adjMatrix,2,3);
-    addEdge(adjMatrix,2,5);
-    addEdge(adjMatrix,2,4);
-    addEdge(adjMatrix,3,5);
-    addEdge(adjMatrix,4,5);
-    addEdge(adjMatrix,5,6);
-    addEdge(adjMatrix,7,4);
-    
-    printAdjMatrix(adjMatrix,vertices);
-    topologicalOrder(adjMatrix, &t,vertices);
-   
-    return 0;
+  int adjMatrix[MAX][MAX];
+  int vertices = 7;
+  Queue t;
+  t.front = t.rear = -1;
+  creategraph(adjMatrix, vertices);
+  addEdge(adjMatrix, 1, 2);
+  addEdge(adjMatrix, 2, 3);
+  addEdge(adjMatrix, 2, 5);
+  addEdge(adjMatrix, 2, 4);
+  addEdge(adjMatrix, 3, 5);
+  addEdge(adjMatrix, 4, 5);
+  addEdge(adjMatrix, 5, 6);
+  addEdge(adjMatrix, 7, 4);
+
+  printAdjMatrix(adjMatrix, vertices);
+  topologicalOrder(adjMatrix, &t, vertices);
+
+  return 0;
 }
