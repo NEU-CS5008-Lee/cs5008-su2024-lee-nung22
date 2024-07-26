a7a99b1668f6990bc3d121a91a513e48d9ee7719
Nicholas Ung <nicholasung22@gmail.com>
2024-07-26 12:33:43 -0700

    Upload lab assignment 12
    Signed-off-by: Nicholas Ung <nicholasung22@gmail.com>

--- lab-assignment12/philosopher-lab.c
+++ lab-assignment12/philosopher-lab.c
@@ -1,48 +1,85 @@
-//Enter your name here
-//Enter your email here
+// Enter your name here
+// Enter your email here
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-pthread_mutex_t chopstick[5];//lock variable
+pthread_mutex_t chopstick[5]; // lock variable
 
 // Thread to be executed
 void *philosopher(void *x)
 {
-    //Treat variable x as a pointer to an int and then extract the value into n
-    int* a=(int*)x;
-    int n=*a;
-    
-    /*-----Insert your code here----*/
-    
-    
-}
+  // Treat variable x as a pointer to an int and then extract the value into n
+  int *a = (int *)x;
+  int n = *a;
 
+  /*-----Insert your code here----*/
+
+  while (1)
+  {
+    // Think
+    printf("Philosopher %d is thinking.\n", n + 1);
+    sleep(n % 2); // Introduce a delay based on the philosopher's index to simulate non-blocking behavior where philosophers don't all start at the same time
+
+    // Pick up chopsticks (mutexes)
+    int left = n;
+    int right = (n + 1) % 5;
+
+    // To avoid deadlock, ensure that all philosophers pick up the lower numbered chopstick first
+    if (n % 2 == 0)
+    {
+      pthread_mutex_lock(&chopstick[left]);
+      pthread_mutex_lock(&chopstick[right]);
+    }
+    else
+    {
+      pthread_mutex_lock(&chopstick[right]);
+      pthread_mutex_lock(&chopstick[left]);
+    }
+
+    // Eat
+    printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n + 1, left, right);
+    sleep(1); // Simulate eating
+
+    // Put down chopsticks (mutexes)
+    pthread_mutex_unlock(&chopstick[left]);
+    pthread_mutex_unlock(&chopstick[right]);
+
+    printf("Philosopher %d finished eating.\n", n + 1);
+
+    break;
+  }
+  return NULL;
+}
 
 /*------------ Main Program---------*/
 int main()
 {
-    int i,val[5];
-    pthread_t threads[5];
-    //initializing the mutex (for chopsticks)
-    for(i=0;i<5;i++){
-        pthread_mutex_init(&chopstick[i],NULL);
-    }   
+  int i, val[5];
+  pthread_t threads[5];
+  // initializing the mutex (for chopsticks)
+  for (i = 0; i < 5; i++)
+  {
+    pthread_mutex_init(&chopstick[i], NULL);
+  }
 
-    //create and run the thread
-    for(i=0;i<5;i++){
-        val[i]=i;
-        pthread_create(&threads[i],NULL,(void *)philosopher,&val[i]);
-    }
-    
-    //wait until all the threads are done
-    for(i=0;i<5;i++){
-        pthread_join(threads[i],NULL);
-    }
-    
-    // Destroying the mutex
-    for(i=0;i<5;i++){
-        pthread_mutex_destroy(&chopstick[i]);
-    }
-    
-    return 0;
+  // create and run the thread
+  for (i = 0; i < 5; i++)
+  {
+    val[i] = i;
+    pthread_create(&threads[i], NULL, (void *)philosopher, &val[i]);
+  }
+
+  // wait until all the threads are done
+  for (i = 0; i < 5; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
+
+  // Destroying the mutex
+  for (i = 0; i < 5; i++)
+  {
+    pthread_mutex_destroy(&chopstick[i]);
+  }
+
+  return 0;
 }
